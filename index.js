c6232b7f0c65ae8886b85539bcd4cc04db68f5f0
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 20:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function getFirstSelector(selector) {
     const firstSelector = document.querySelector(selector)
     return firstSelector;
 }
+
+function nestedTarget() {
+    const nested = document.getElementById("nested").querySelector("div.target")
+}
