748fd0842d8829df40eb7bbc3f25fcf25b6dfe6d
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 20:39:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector) {
+    const firstSelector = document.querySelector(selector)
+    return firstSelector;
+}
 
+function nestedTarget() {
+    const nested = document.getElementById("nested").querySelector("div.target")
+    return nested
+}
+
+function increaseRankBy(n) {
+    const nodeList = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].innerHTML = parseInt(nodeList[i].innerHTML) + n;
+    }
+}
+
+function deepestChild() {
+
+}
+
+function deepestChild() {
+    let node = document.getElementById("grand-node");
+    let nextNode = node.children[0];
+
+    while (nextNode) {
+        node = nextNode;
+        nextNode = node.children[0];
+    }
+    return node;
+}
