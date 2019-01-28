9872f325096de87c7b60c276bd97023c76ed77a4
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 20:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function nestedTarget() {
     const nested = document.getElementById("nested").querySelector("div.target")
     return nested
 }
+
+function increaseRankBy(n) {
+    const nodeList = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].innerHTML = (i + n).toString()
+    }
+}
